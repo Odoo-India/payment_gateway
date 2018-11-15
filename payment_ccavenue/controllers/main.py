9b64038031a1f1868f2817074e0cf3a307367be6
Jinal Patel <jip@odoo.com>
2018-11-15 15:21:56 +0530

    [IMP] payment_ccavenue: various improvement

--- payment_ccavenue/controllers/main.py
+++ payment_ccavenue/controllers/main.py
@@ -4,6 +4,7 @@ import logging
 import pprint
 import werkzeug
 
+from odoo.addons.payment_ccavenue.models.ccavutil import decrypt
 from odoo import http
 from odoo.http import request
 
@@ -11,13 +12,22 @@ _logger = logging.getLogger(__name__)
 
 
 class CCAvenueController(http.Controller):
+    _return_url = '/payment/ccavenue/return/'
 
     @http.route(['/payment/ccavenue/return', '/payment/ccavenue/cancel'], type='http', auth='public', csrf=False)
     def ccavenue_return(self, return_url=False, **post):
         _logger.info('CCAvenue: Entering form_feedback with post data %s', pprint.pformat(post))
         if post:
             PaymentAcquirer = request.env['payment.acquirer']
-            key = PaymentAcquirer.search([('provider', '=', 'ccavenue')], limit=1).ccavenue_working_key
-            post = PaymentAcquirer._ccavenue_encrypted_response(post, key)
-            request.env['payment.transaction'].sudo().form_feedback(post, 'ccavenue')
-        return werkzeug.utils.redirect(return_url or "/")
+            workingKey = PaymentAcquirer.search([('provider', '=', 'ccavenue')], limit=1).ccavenue_working_key
+            # encResp = post.get('encResp')
+            result = decrypt(post.get('encResp'), workingKey)
+            post_result = {}
+            vals = result.split('&')
+            for data in vals:
+                temp = data.split('=')
+                post_result[temp[0]] = temp[1]
+
+            request.env['payment.transaction'].sudo().form_feedback(post_result, 'ccavenue')
+        # return werkzeug.utils.redirect(return_url or "/")
+        return werkzeug.utils.redirect('/payment/process')
